a9fad09957e8761476cb8f67c2babd648aa9deac
Ayomide Lawale <ayomidelaw01@gmail.com>
2026-01-04 23:06:48 +0100

    feat: initialize entry point and  error handling

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <nav class="bg-white shadow p-4 flex gap-4">
+    <h1 class="font-bold">Markdown Preview App</h1>
+    <div class="ml-auto flex gap-4">
+      <RouterLink to="/" class="hover:text-indigo-600" active-class="font-bold text-indigo-600">
+        Home
+      </RouterLink>
+      <RouterLink to="/error-test" class="hover:text-indigo-600" active-class="font-bold text-indigo-600">
+        Error Test
+      </RouterLink>
+    </div>
+  </nav>
+
+  <RouterView />
+</template>
